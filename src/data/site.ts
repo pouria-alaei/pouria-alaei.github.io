65eb30215270e188773949393120a35445b421b3
Pooria Alaei <pooria.alaei1994@gmail.com>
2026-06-26 02:01:15 +0330

    Add LinkedIn profile link

--- src/data/site.ts
+++ src/data/site.ts
@@ -11,5 +11,5 @@ export const site = {
   email: "pouria.alaei.dev@gmail.com",
   github: "https://github.com/pouria-alaei",
   telegram: "https://t.me/Pooria_Alaei",
-  linkedin: "https://www.linkedin.com/in/PUT-YOUR-LINKEDIN-USERNAME-HERE",
+  linkedin: "https://www.linkedin.com/in/pouria-alaeinezhad/",
 };
