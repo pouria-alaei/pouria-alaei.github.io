a2110e64d0d7d74d38271e90fcc852be27540e51
Pooria Alaei <pooria.alaei1994@gmail.com>
2026-06-26 01:28:06 +0330

    Add Telegram and LinkedIn contact links

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,5 +2,5 @@ export const contact = {
   email: "pouria.alaei.dev@gmail.com",
   github: "https://github.com/pouria-alaei",
   telegram: "https://t.me/Pooria_Alaei",
-  linkedin: "https://www.linkedin.com/in/PUT-YOUR-LINKEDIN-USERNAME-HERE",
+  linkedin: "https://www.linkedin.com/in/pouria-alaeinezhad/",
 };
